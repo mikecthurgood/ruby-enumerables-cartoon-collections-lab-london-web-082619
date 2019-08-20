d6069c4a2219a2f0c5e9a34c0b8140a97e625654
Mike Thurgood <mikecthurgood@gmail.com>
2019-08-20 14:16:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,5 +19,5 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.any? { | snacks | 
+  array.find { | snacks | 
 end
