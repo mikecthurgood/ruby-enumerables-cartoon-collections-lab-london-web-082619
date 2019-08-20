ac4933cfa9e4a5602f0653de143f20b059eec180
Mike Thurgood <mikecthurgood@gmail.com>
2019-08-20 13:59:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,8 @@ def roll_call_dwarves(names)
   end
 end
 
-def summon_captain_planet
-  # Your code here
+def summon_captain_planet(string)
+  string.map 
 end
 
 def long_planeteer_calls# code an argument here
