b98d3748f6513eec69bef54232143944b061de28
Mike Thurgood <mikecthurgood@gmail.com>
2019-08-20 14:05:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(names)
 end
 
 def summon_captain_planet(string)
-  string.map { | call | call.capitalize +'!'}
+  string.map { | call | return " #{call.capitalize}!"}
 end
 
 def long_planeteer_calls# code an argument here
