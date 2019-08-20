76e262df761ace30e232d9a65af33ac020aa5762
Mike Thurgood <mikecthurgood@gmail.com>
2019-08-20 14:11:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,8 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.any?
+  array.any? { | call | call.length > 4 }
+  return true
 end
 
 def find_the_cheese# code an argument here
